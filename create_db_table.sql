e7a80ede7da583086c46fd9e8ae71e1e0460fa74
ShineLine <shineline@AnotherWay>
2015-10-27 02:41:05 +0400

    second_edition

---
+++ create_db_table.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE `working_tasks` CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE TABLE `regs` (
+ `user_id` int(11) NOT NULL default '0',
+ `login` varchar(50) NOT NULL default '',
+ `reg_date` datetime NOT NULL default '0000-00-00 00:00:00',
+ PRIMARY KEY  (`user_id`),
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='test task table for the UNION-IT'
